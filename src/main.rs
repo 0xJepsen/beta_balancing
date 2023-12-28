d3048f48319e2f83285695277401a7058ec8d737
Waylon Jepsen <waylonjepsen1@gmail.com>
2023-12-28 12:53:56 -0700

    async wrapping

--- src/main.rs
+++ src/main.rs
@@ -9,22 +9,28 @@ async fn main() -> Result<()> {
     println!("Positions: {:#?}", portfolio.positions);
     println!("Target weights: {:#?}", portfolio.target_weights);
     println!("Rebalance type: {:#?}", portfolio.rebalance_type);
-
     let start_time = std::time::Instant::now();
     println!("actual weights:{:#?} ", portfolio.get_actual_weights().await?);
     let duration = start_time.elapsed();
     println!("Time taken to update weights: {:?}", duration);
     println!("Portfolio value: {}", portfolio.get_portfolio_value());
+
+    let historical_prices = get_historical_daily_prices(5, "ethereum").await?;
+    println!("Historical prices: {:?}", historical_prices);
+
     Ok(())
 }
 
-pub fn get_historical_daily_prices(number_of_days: i64, id: &str) -> Result<Vec<f64>> {
-    let eth_historical_price = rust_gecko::coins::market_chart(
-        id,
-        "usd",
-        (number_of_days - 1).to_string().as_str(),
-        Some("daily"),
-    );
+pub async fn get_historical_daily_prices(number_of_days: i64, id: &str) -> Result<Vec<f64>> {
+    let id = id.to_string(); // Clone id here
+    let eth_historical_price = tokio::task::spawn_blocking(move || {
+        rust_gecko::coins::market_chart(
+            &id,
+            "usd",
+            (number_of_days - 1).to_string().as_str(),
+            Some("daily"),
+        )
+    }).await?;
     let json = eth_historical_price.json.clone().unwrap();
     // can also parse the daily market caps and total volumes from this repsonse
     let prices: Vec<_> = json
@@ -38,8 +44,10 @@ pub fn get_historical_daily_prices(number_of_days: i64, id: &str) -> Result<Vec<
     Ok(prices)
 }
 
-pub fn get_eth_price() -> Result<f64> {
-    let res = rust_gecko::simple::price(vec!["ethereum"], vec!["usd"], None, None, None, None);
+pub async fn get_eth_price() -> Result<f64> {
+    let res = tokio::task::spawn_blocking(move || {
+        rust_gecko::simple::price(vec!["ethereum"], vec!["usd"], None, None, None, None)
+    }).await?;
     match res.json {
         Some(json) => {
             let eth_price = json.get("ethereum").and_then(|eth| eth.get("usd")).unwrap();
